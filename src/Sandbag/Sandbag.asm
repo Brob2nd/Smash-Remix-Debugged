3b2561d68da451f5fb88bc9f7134462654e61b38
Krix08 <60101773+Kriix08@users.noreply.github.com>
2025-06-30 04:46:47 +0300

    Add Sandbag Chant

--- src/Sandbag/Sandbag.asm
+++ src/Sandbag/Sandbag.asm
@@ -74,11 +74,9 @@ scope Sandbag {
     Character.edit_action_parameters(SANDBAG, Action.Taunt,                   -1,                                   0x80000000,                 -1)
 
     // Modify Actions            // Action              // Staling ID    // Main ASM          // Interrupt/Other ASM          // Movement/Physics ASM         // Collision ASM
-
     Character.edit_action(SANDBAG, 0xE0,                0x01,            0x00000000,          0x8013E070,                     0x800D8BB4,                     0x800DDEE8)
 
     // Modify Menu Action Parameters             // Action      // Animation                // Moveset Data             // Flags
-
     Character.edit_menu_action_parameters(SANDBAG, 0x0,           File.SANDBAG_IDLE,       0x80000000,                   -1)
     Character.edit_menu_action_parameters(SANDBAG, 0x1,           File.SANDBAG_IDLE,       0x80000000,                   -1)
     Character.edit_menu_action_parameters(SANDBAG, 0x2,           File.SANDBAG_IDLE,       0x80000000,                   -1)
@@ -92,6 +90,11 @@ scope Sandbag {
 
     // Shield colors for costume matching
     Character.set_costume_shield_colors(SANDBAG, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, NA, NA)
+
+    // Set crowd chant FGM.
+    Character.table_patch_start(crowd_chant_fgm, Character.id.SANDBAG, 0x2)
+    dh  0x53D
+    OS.patch_end()
 
     // Set Remix 1P ending music
     Character.table_patch_start(remix_1p_end_bgm, Character.id.SANDBAG, 0x2)
