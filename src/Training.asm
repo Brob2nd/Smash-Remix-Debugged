6d5c7d176490ccd6e5dfa953edce96c9ae248edf
Brobgonal Second <yulianaacostats@gmail.com>
2026-03-10 19:45:48 -0500

    Changed Back Training Mode's song position

--- src/Training.asm
+++ src/Training.asm
@@ -3987,6 +3987,7 @@ scope Training {
     Menu.entry("Port:", Menu.type.INT, 1, 1, 4, OS.NULL, OS.NULL, OS.NULL, OS.NULL, tail_p1)
 
     string_table_music:
+    dw       Toggles.entry_random_music_training_mode + 0x28
     dw       Toggles.entry_random_music_bonus + 0x28
     dw       Toggles.entry_random_music_congo_jungle + 0x28
     dw       Toggles.entry_random_music_credits + 0x28
@@ -4002,7 +4003,6 @@ scope Training {
     dw       Toggles.entry_random_music_planet_zebes + 0x28
     dw       Toggles.entry_random_music_saffron_city + 0x28
     dw       Toggles.entry_random_music_sector_z + 0x28
-    dw       Toggles.entry_random_music_training_mode + 0x28
     dw       Toggles.entry_random_music_yoshis_island + 0x28
     evaluate total(17)
     evaluate n(0x2F)
@@ -4017,6 +4017,7 @@ scope Training {
     }
 
     bgm_table:
+    dh      BGM.special.TRAINING
     dh      BGM.menu.BONUS
     dh      BGM.stage.CONGO_JUNGLE
     dh      BGM.menu.CREDITS
@@ -4032,7 +4033,6 @@ scope Training {
     dh      BGM.stage.PLANET_ZEBES
     dh      BGM.stage.SAFFRON_CITY
     dh      BGM.stage.SECTOR_Z
-    dh      BGM.special.TRAINING
     dh      BGM.stage.YOSHIS_ISLAND
     evaluate n(0x2F)
     while {n} < MIDI.midi_count {
@@ -4125,7 +4125,7 @@ scope Training {
 
     entry_shield_break_mode:; Menu.entry("Shield Break Mode:", Menu.type.INT, 0, 0, 2, OS.NULL, OS.NULL, string_table_shield_break, OS.NULL, entry_oos_option)
     entry_oos_option:; Menu.entry("OOS Action:", Menu.type.INT, 0, 0, OOS_MAX, OS.NULL, OS.NULL, string_table_oos_options, OS.NULL, entry_music)
-    entry_music:; Menu.entry("Music:", Menu.type.INT, 15, 0, {total} - 1, play_bgm_, OS.NULL, string_table_music, OS.NULL, entry_bg)
+    entry_music:; Menu.entry("Music:", Menu.type.INT, 0, 0, {total} - 1, play_bgm_, OS.NULL, string_table_music, OS.NULL, entry_bg)
     entry_bg:; Menu.entry_bool("Stage Background:", OS.FALSE, entry_tech_behavior)
     entry_tech_behavior:; Menu.entry("CPU Teching:", Menu.type.INT, 0, 0, TECH_MAX, OS.NULL, OS.NULL, string_table_tech_options, OS.NULL, entry_di_type)
     entry_di_type:; Menu.entry("CPU DI Type:", Menu.type.INT, 0, 0, DI_TYPE_MAX, OS.NULL, OS.NULL, string_table_di_type_options, OS.NULL, entry_di_strength)
